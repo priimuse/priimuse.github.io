c1eed7b6c3d8fe56ecdda5d858187ffb632d05d6
priimuse <wmwilson87@gmail.com>
2016-06-07 11:20:08 -0700

    Update script_01.js

--- script_01.js
+++ script_01.js
@@ -1,1 +1,18 @@
 console.log("Hi World!");
+
+//wit.ai functionality
+var $;
+var server_token = 5RZS53RK4KKUSQ2MBL3JFLLSXTPWFHS3;
+
+$.ajax({
+  url: 'https://api.wit.ai/message',
+  data: {
+    'q': 'set an alarm in 10min',
+    'access_token' : server_token
+  },
+  dataType: 'jsonp',
+  method: 'GET',
+  success: function(response) {
+      console.log("success!", response);
+  }
+});
